51b151c9fb57550468df768b41fe066c7a671409
Josip <josip.relja5@gmail.com>
2023-06-08 23:51:47 +0200

    dsadsa

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const express = require('express');
-const fetch = require('node-fetch');
+const express = require("express")
+const app = express()
 
-const app = express();
-const PORT = 3000;
+require('dotenv').config()
 
-app.get('/api/proxy', async (req, res) => {
-  try {
-    const response = await fetch('https://diplomski-api.vercel.app/api/v1/oglasi/prikazoglasi');
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred' });
-  }
-});
+app.use(express.json())
 
-app.listen(PORT, () => {
-  console.log(`Proxy server listening on port ${PORT}`);
-});
+
+const oglasiRouter = require('./routes/oglasi.router')
+
+app.use("/api/v1/oglasi", oglasiRouter)
+
+app.listen(process.env.PORT, () => console.log("Server is running on port 5000"))
